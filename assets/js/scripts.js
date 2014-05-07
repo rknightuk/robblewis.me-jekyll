b86158218a20848e5ad7a633ebbc0a328944b7bf
Robb Lewis <me@therobb.com>
2014-05-07 18:16:24 +0100

    Remove instagram from home page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,19 +5,5 @@ $(document).ready(function(){
 		e.preventDefault();
 		$('.burger-menu').slideToggle('fast');
 	});
-
-	// Get Instagram
-	if ($('.instagram').length == 1) {
-		$.ajax({
-			url:'https://api.instagram.com/v1/users/433244/media/recent?access_token=433244.467ede5.8d80ee8b78b3498891ce1ae5dce70931&count=5',
-			dataType:'jsonp',
-			success: function(data) {
-				var grams = data.data;
-				for (i = 0; i < grams.length; i++) {
-					$('#instagram').append('<a class="gram-link" href="'+ grams[i].link +'"><div class="gram"><img width="150" height="150" src="'+ grams[i].images.thumbnail.url +'" alt="'+ grams[i].caption.text +'"></div></a>');
-				}
-			}
-		});
-	}
 	
 });
